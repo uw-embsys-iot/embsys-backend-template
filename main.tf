1fefed91d0f8467d01471ac5c277cee4c7a22649
Mikhail Skobov <skobovm@gmail.com>
2023-05-13 14:43:41 -0700

    Functioning assignment 3

--- main.tf
+++ main.tf
@@ -35,10 +35,19 @@ resource "aws_security_group" "allow_all" {
   description = "Allow all inbound traffic"
 
   ingress {
-    description      = "All traffic"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Inbound"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
