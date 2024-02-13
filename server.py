34cbffaca2ccca949e3f1145157cb691a33169f3
Mikhail Skobov <mskobov@lyft.com>
2024-02-13 15:07:57 -0800

    Implementing solution

--- server.py
+++ server.py
@@ -1,5 +1,15 @@
-import sockets
 import socketserver
+
+class MyTCPHandler(socketserver.StreamRequestHandler):
+    def handle(self):
+        print("TCP handler")
+
+        # self.request is the TCP socket connected to the client
+        self.data = self.rfile.readline().strip()
+        print("{} sent:".format(self.client_address[0]))
+        print(self.data)
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
 
 if __name__ == "__main__":
     # The host works differently depending on the platform, so the
@@ -8,7 +18,10 @@ if __name__ == "__main__":
     HOST, PORT = "", 4242
     print("The server is running")
 
-    # IOTEMBSYS4: Implement your TCP server here
+    # Create the server, binding to localhost on port 9999
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
     
     print("The server is shutting down")
-  
