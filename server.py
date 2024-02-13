666b8b2d4cf70d1d691dfddf1a9dd6b8e07bc54f
Mikhail Skobov <mskobov@lyft.com>
2024-02-13 15:07:21 -0800

    Adding server.py file

---
+++ server.py
@@ -0,0 +1,14 @@
+import sockets
+import socketserver
+
+if __name__ == "__main__":
+    # The host works differently depending on the platform, so the
+    # best bet is to leave it blank. If you have having trouble
+    # connecting, you can try "localhost" or "127.0.0.1"
+    HOST, PORT = "", 4242
+    print("The server is running")
+
+    # IOTEMBSYS4: Implement your TCP server here
+    
+    print("The server is shutting down")
+  
