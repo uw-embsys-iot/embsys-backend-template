da19365f6ea07991fb513ec6b1b29fb31d79c705
Mikhail Skobov <mskobov@lyft.com>
2024-02-13 15:11:57 -0800

    Change comments to assignment 4

--- main.tf
+++ main.tf
@@ -24,13 +24,13 @@ data "aws_key_pair" "ssh_key" {
 
   filter {
     name   = "key-pair-id"
-    # IOTEMBSYS2: Copy the keypair ID from the current AWS lab.
+    # IOTEMBSYS4: Copy the keypair ID from the current AWS lab.
     # Note: this changes with every new Vocareum lab!
     values = ["YOUR-KEY-HERE"]
   }
 }
 
-# IOTEMBSYS2: create a security group that limits ingress traffic to SSH and port 8080
+# IOTEMBSYS4: create a security group that limits ingress traffic to your socket echo server.
 resource "aws_security_group" "allow_all" {
   name        = "allow_all"
   description = "Allow all inbound traffic"
@@ -70,7 +70,6 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0557a15b87f6559cf"
   instance_type = "t2.micro"
   key_name      = "vockey"
-  # IOTEMBSYS2: set the security group here, once created
   vpc_security_group_ids = [aws_security_group.allow_ssh_and_tcp.id]
 
   tags = {
@@ -78,7 +77,6 @@ resource "aws_instance" "app_server" {
   }
 }
 
-# IOTEMBSYS2: Create an output target that prints the instance public IP.
 output "instance_public_ip" {
     value = aws_instance.app_server.public_ip
     description = "AWS EC2 Instance Public IP"
